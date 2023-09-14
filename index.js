3e3dfa878d1613a70dd1e1100fab79efd73d63b5
PRich57 <pcrichards57@gmail.com>
2023-09-14 15:50:41 -0600

    Add questions to the questions array and create markdown

--- index.js
+++ index.js
@@ -1,9 +1,66 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'What was your motivation for this project?',
+    name: 'motivation',
+  },
+  {
+    type: 'input',
+    message: 'Why did you build this project?',
+    name: 'build',
+  },
+  {
+    type: 'input',
+    message: 'What problem does your project solve?',
+    name: 'problem',
+  },
+  {
+    type: 'input',
+    message: 'What did you learn?',
+    name: 'learned',
+  },
+  {
+    type: 'input',
+    message: 'What do you plan to implement in the future?',
+    name: 'implement',
+  },
+  {
+    type: 'input',
+    message: 'If deployed, please provide a link to the live site.',
+    name: 'deploy',
+  },
+  {
+    type: 'editor',
+    message: 'In your default editor, please describe the steps required to install your project.',
+    name: 'installation',
+  },
+  {
+    type: 'editor',
+    message: 'In your default editor, please provide instructions for the usage of your project.',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'If you created an application or package and would like other developers to contribute it, please include guidelines for how to do so.',
+    name: 'contribution',
+  },
+  {
+    type: 'input',
+    message: 'If you have written any tests for your application please provide examples of how to run them here.',
+    name: 'tests',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,47 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+
+  - ${data.motivation}
+  - ${data.build}
+  - ${data.problem}
+  - ${data.learned}
+  - ${data.implement}
+  - ${data.deploy}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+
+  ${data.license}
+
+  ## Contributing
+
+ ${data.contribution}
+  
+  ## Tests
+
+${data.tests}
+
+  ## Questions
+
+  `;
 }
 
 module.exports = generateMarkdown;
